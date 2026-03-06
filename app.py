696febc1ad74f78b4e04a55becb15bdb0f70e3e1
blah161 <63870134+blah161@users.noreply.github.com>
2026-03-06 05:22:10 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import os
+import importlib
+
+app = Flask(__name__)
+
+TOOLS = []
+
+tools_dir = "tools"
+
+if os.path.exists(tools_dir):
+    for tool in os.listdir(tools_dir):
+
+        module_path = f"tools.{tool}.routes"
+
+        module = importlib.import_module(module_path)
+
+        blueprint = getattr(module, tool)
+
+        app.register_blueprint(blueprint, url_prefix=f"/{tool}")
+
+        TOOLS.append({
+            "name": tool.capitalize(),
+            "path": f"/{tool}"
+        })
+
+
+@app.route("/")
+def dashboard():
+    return render_template("dashboard.html", tools=TOOLS)
